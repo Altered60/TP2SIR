0756a0a6846b842f767827f239c966066c936047
Guillaume <ghaye60@gmail.com>
2017-04-05 11:18:52 +0200

    test

---
+++ tpJpa/src/main/java/test/testjpa/domain/SmartDevice.java
@@ -0,0 +1,37 @@
+//package test.testjpa.domain;
+//
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.ManyToOne;
+//
+//@Entity
+//public class SmartDevice {
+//	
+//	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+//	protected Long id;
+//	
+//	protected Residence residence;
+//	
+//	
+//	public Long getId() {
+//        return id;
+//    }
+//	
+//	public void setId(Long id) {
+//        this.id = id;
+//    }
+//	
+//
+//    public Residence getResidence() {
+//        return this.residence;
+//    }
+//
+//	public void setResidence(Residence residence) {
+//		this.residence = residence;
+//	}
+//}
+//	
+
